4257943ec6fe8524ce22d0bc422ab3e7f8a8ffd9
Dimple Chauhan <dimplechauhan312@gmail.com>
2026-03-04 17:14:16 +0530

    Time: 2 ms (94.84%), Space: 47.4 MB (26.69%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.java
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.java
@@ -5,11 +5,9 @@ class Solution {
         int cnt=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(mat[i][j]==1){
-                if(nocolumn(mat,i,j) && norow(mat,i,j)){
+                if(mat[i][j]==1 && nocolumn(mat,i,j) && norow(mat,i,j)){
                     cnt++;
                 }
-            }
             }
         }
         return cnt;
