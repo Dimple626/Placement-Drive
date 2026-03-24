4698a63dfd5e3c7c2ff917c7e9524bb2da677ba5
Dimple Chauhan <dimplechauhan312@gmail.com>
2026-03-24 14:47:03 +0530

    Time: 7 ms (60.42%), Space: 47.3 MB (41.76%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean canFinish(int v, int[][] edges) {
+          ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<v;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int[] edge:edges){
+            adj.get(edge[1]).add(edge[0]);
+        }
+        int[] indegree=new int[v];
+        for(int i=0;i<v;i++){
+            for(int it:adj.get(i)){
+                indegree[it]++;
+            }
+        }
+        Queue<Integer> q=new LinkedList<>();
+        for(int i=0;i<v;i++){
+            if(indegree[i]==0){
+                q.add(i);
+            }
+        }
+        ArrayList<Integer> ans=new ArrayList<>();
+        while(!q.isEmpty()){
+            int node=q.peek();
+            q.remove();
+            ans.add(node);
+            for(int it:adj.get(node)){
+                indegree[it]--;
+                if(indegree[it]==0){
+                q.add(it);
+            }
+            }
+        }
+        return ans.size()==v;
+        
+    }
+}
