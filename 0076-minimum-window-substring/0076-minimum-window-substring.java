3ee1df98aadbc68e639850547d3bd29f6988ed10
Dimple Chauhan <dimplechauhan312@gmail.com>
2026-04-17 14:33:59 +0530

    Time: 2 ms (99.94%), Space: 45.6 MB (80.28%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,15 +1,15 @@
 class Solution {
     public String minWindow(String s, String t) {
         if(s.length()<t.length()) return "";
-        int[] freq=new int[128];
-        int right=0;
         int start=0;
+        int right=0;
         int left=0;
-        int minlen=Integer.MAX_VALUE;
-        int required=t.length();
-        for(char ch:t.toCharArray()){
-            freq[ch]++;
-        }
+        int[] freq=new int[128];
+       for(char ch:t.toCharArray()){
+        freq[ch]++;
+       }
+       int minlen=Integer.MAX_VALUE;
+       int required=t.length();
         while(right<s.length()){
             freq[s.charAt(right)]--;
             if(freq[s.charAt(right)]>=0){
@@ -20,12 +20,12 @@ class Solution {
                     minlen=right-left+1;
                     start=left;
                 }
-            char l=s.charAt(left);
-            freq[l]++;
-            if(freq[l]>0){
-                required++;
-            }
-            left++;
+                char ch=s.charAt(left);
+                freq[ch]++;
+                if(freq[ch]>0){
+                    required++;
+                }
+                left++;
             }
             right++;
         }
