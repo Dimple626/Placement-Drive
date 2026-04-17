7b42b1a0333d90eb83a64305de83cfeafe9836b3
Dimple Chauhan <dimplechauhan312@gmail.com>
2026-04-17 16:21:18 +0530

    Time: 1 ms (99.89%), Space: 45.2 MB (82.53%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int compress(char[] chars) {
+    int index=0;
+    int i=0;
+    while(i<chars.length){
+        char current=chars[i];
+        int count=0;
+        while(i<chars.length && chars[i]==current){
+            count++;
+            i++;
+        }
+        chars[index++]=current;
+        if(count>1){
+            String num=String.valueOf(count);
+            for(char ch:num.toCharArray()){
+                chars[index++]=ch;
+            }
+        }
+    }
+        return index;
+    
+    
+      
+    }
+}
