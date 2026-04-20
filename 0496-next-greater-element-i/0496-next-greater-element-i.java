8404635355929f57121bd6150fc589ee4d8581a8
Dimple Chauhan <dimplechauhan312@gmail.com>
2026-04-20 13:24:51 +0530

    Time: 7 ms (5.34%), Space: 45.1 MB (70.46%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int n=nums1.length;
+        int[] res=new int[n];
+        int[] copy = nums2.clone();
+        Stack<Integer> st=new Stack<>();
+        int m=nums2.length;
+        for(int i=m-1;i>=0;i--){
+            if(st.isEmpty()){
+                nums2[i]=-1;
+            }
+            while(!st.isEmpty() && nums2[i]>st.peek()){
+                st.pop();
+            }
+            
+        nums2[i]=st.isEmpty()?-1:st.peek();
+            
+            st.push(copy[i]);
+        }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(nums1[i]==copy[j]){
+                res[i]=nums2[j];
+            }
+        }
+    }
+        return res;
+    }
+}
