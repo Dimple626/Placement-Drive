d09446608d89e1f8ceb41d70b14af7a216ec1a62
Dimple Chauhan <dimplechauhan312@gmail.com>
2026-04-20 13:34:12 +0530

    Time: 4 ms (74.92%), Space: 45.6 MB (5.45%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,26 +1,19 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         int n=nums1.length;
-        int[] res=new int[n];
-        int[] copy = nums2.clone();
+        HashMap<Integer,Integer> map=new HashMap<>();
         Stack<Integer> st=new Stack<>();
-        int m=nums2.length;
-        for(int i=m-1;i>=0;i--){
+        for(int i=nums2.length-1;i>=0;i--){
             while(!st.isEmpty() && nums2[i]>st.peek()){
                 st.pop();
             }
-            
-        nums2[i]=st.isEmpty()?-1:st.peek();
-            
-            st.push(copy[i]);
+            map.put(nums2[i],st.isEmpty()?-1:st.peek());
+            st.push(nums2[i]);
         }
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            if(nums1[i]==copy[j]){
-                res[i]=nums2[j];
-            }
+        int[] res=new int[n];
+        for(int i=0;i<n;i++){
+            res[i]=map.get(nums1[i]);
         }
-    }
         return res;
     }
 }
