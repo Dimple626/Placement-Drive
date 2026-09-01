4f793a398a76c825e0fe86a58c4913b36c1dc830
Dimple Chauhan <dimplechauhan312@gmail.com>
2026-09-02 00:01:34 +0530

    Time: 0 ms (100%), Space: 43.2 MB (50.92%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
@@ -1,31 +1,12 @@
 class Solution {
     public int timeRequiredToBuy(int[] tickets, int k) {
-
-        Queue<Integer> q = new LinkedList<>();
-
-        // Put all person's indexes into queue
-        for(int i = 0; i < tickets.length; i++) {
-            q.add(i);
-        }
-
-        int time = 0;
-
-        while(true) {
-
-            // Take person from front
-            int person = q.remove();
-
-            // They buy 1 ticket
-            tickets[person]--;
-            time++;
-
-            // If this was person k and they finished
-            if(person == k && tickets[person] == 0) {
-                return time;
-            }
-            if(tickets[person] > 0) {
-                q.add(person);
-            }
-        }
+        int cnt=0;
+        for(int i=0;i<tickets.length;i++){
+            if(i<=k){
+            cnt+=Math.min(tickets[k],tickets[i]);
+        }else{
+            cnt+=Math.min(tickets[k]-1,tickets[i]);
+        }}
+        return cnt;
     }
 }
