16c22914a980af3dc75793729f0cd776c37013b1
Dimple Chauhan <dimplechauhan312@gmail.com>
2026-09-13 10:15:07 +0530

    Time: 1 ms (98.04%), Space: 46.8 MB (94.24%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,18 +1,22 @@
 class Solution {
-    public int[][] insert(int[][] intervals, int[] newinterval) {
-        int[][] result=new int[intervals.length+1][2];
-        int i=0,j=0;
-        while(i<intervals.length && intervals[i][1]<newinterval[0]){
-            result[j++]=intervals[i++];
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        ArrayList<int[]> list=new ArrayList<>();
+        int i=0;
+        int n=intervals.length;
+        while(i<n && newInterval[0]>intervals[i][1]){
+            list.add(intervals[i]);
+            i++;
         }
-        while(i<intervals.length && intervals[i][0]<=newinterval[1]){
-            newinterval[0]=Math.min(newinterval[0],intervals[i][0]);
-            newinterval[1]=Math.max(newinterval[1],intervals[i++][1]);
+          while(i<n && newInterval[1]>=intervals[i][0]){
+           newInterval[0]=Math.min(newInterval[0],intervals[i][0]);
+            newInterval[1]=Math.max(newInterval[1],intervals[i][1]);
+            i++;
         }
-        result[j++]=newinterval;
-        while(i<intervals.length){
-            result[j++]=intervals[i++];
+        list.add(newInterval);
+         while(i<n){
+            list.add(intervals[i]);
+            i++;
         }
-        return java.util.Arrays.copyOf(result,j);
+        return list.toArray(new int[list.size()][2]);
     }
 }
