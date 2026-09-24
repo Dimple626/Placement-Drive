c0250d9e649b3a953eea3879494d2d32f18e1d84
Dimple Chauhan <dimplechauhan312@gmail.com>
2026-09-25 01:03:17 +0530

    Time: 58 ms (5.03%), Space: 48 MB (13.9%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,21 +1,13 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-
         HashMap<Integer, Integer> map = new HashMap<>();
-
-        // Count frequency
         for (int num : nums) {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
-
         int[] arr = new int[k];
-
         for (int i = 0; i < k; i++) {
-
             int maxFreq = 0;
             int maxKey = 0;
-
-            // Find maximum frequency
             for (int key : map.keySet()) {
                 if (map.get(key) > maxFreq) {
                     maxFreq = map.get(key);
@@ -24,8 +16,6 @@ class Solution {
             }
 
             arr[i] = maxKey;
-
-            // Remove so we don't select it again
             map.remove(maxKey);
         }
 
